ca740c0a61957f790a1ae749932220a7f3539783
aaaneigeideitao <3120302052@qq.com>
2018-11-01 23:16:13 +0800

    study Mutation and Action

--- src/store.js
+++ src/store.js
@@ -2,15 +2,20 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-
+// function test (timeout) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(timeout), timeout)
+//   })
+// }
 export default new Vuex.Store({
-  // 状态
+  // state用于存储状态
+  // 单一状态树
   state: {
     isLogin: false,
     users: [
       {
         id: 0,
-        username: 'jwy',
+        name: 'jwy',
         age: 23
       },
       {
@@ -24,20 +29,60 @@ export default new Vuex.Store({
         age: 3
       }
     ],
-    count: 0
-
+    count: 0,
+    currentUser: null
   },
+
   // 如果你要改变store中的状态，请在mutations的定义那些方法，然后在组件中使用store实例的commit(mutations)调用
+  // mutation 都是同步操作
+  // 大型开发中请用常量替代 Mutation 事件类型
   mutations: {
     changeLoginState (state) {
       state.isLogin = !state.isLogin
     },
     changeCount (state, num) {
       state.count += num
-    }
-  },
-  actions: {
+    },
+    // 建议通过对象来传参
+    reduceCount (state, payload) {
+      state.count -= payload.reduceSize
+    },
 
+    // 测试异步用
+    mutationA (state) {
+      state.isLogin = true
+    },
+    mutationB (state, payload) {
+      console.log(payload)
+      state.currentUser = state.users[payload.index]
+    }
+
+  },
+
+  // Action类似于mutation
+  // Action提交的是mutation，而不是直接变更状态
+  // Action可以包含任意异步操作
+  actions: {
+    reduceCountAsync ({ commit }, payload) {
+      setTimeout(() => {
+        commit('reduceCount', {
+          reduceSize: payload.reduceSize
+        })
+      }, 1000)
+    },
+    loginAsync ({ commit }) {
+      setTimeout(() => {
+        commit('changeLoginState')
+      }, 2000)
+    }
+    // 组合action
+    // async actionA ({ commit }) {
+    //   await test(3000)
+    // },
+    // async actionB ({ dispatch, commit }, payload) {
+    //   await dispatch('actionA')
+    //   await commit('mutationB', payload.index)
+    // }
   },
 
   // getters:存放派生出的属性和方法
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,39 @@
   <div class="home">
     <div>{{localComputed}}</div>
     <!-- <div>{{isLogin}}</div> -->
-    <div>登陆状态:{{isNotLogin?'未登陆':'已登陆'}}</div>
-    <button @click="login">登陆</button>
-    <div>{{isNotLoginAlias}}</div>
-    <ul>
-      <li v-for="(user,index) in adults" :key="index">
-        {{user.name}}:{{user.age}}
-      </li>
-    </ul>
+    <div>登陆状态:{{isNotLogin?'未登陆':'已登陆'}}
+      <p>
+        <button @click="login">登陆</button>
+        <button @click="loginAsync">异步登陆</button>
+      </p>
+    </div>
+    <!-- <div>{{isNotLoginAlias?'未登陆':'已登陆'}}
+        <p>计算属性别名并不会更新，只是一个副本</p>
+    </div> -->
+    <div>{{countAlias}}</div>
+    <div>
+      <ul>
+        <li v-for="(user,index) in adults" :key="index">
+          {{user.name}}:{{user.age}}
+        </li>
+      </ul>
+    </div>
     <div>{{getUserById(1)}}</div>
     <div>
-      <button @click="changeCount">+10</button>
-      {{count}}
+        <p>count:{{count}}</p>
+        <p>
+         <button @click="changeCount">增加10</button>
+        </p>
+        <p>
+        设置减量:<input type="text" v-model="reduceSize" >
+        <button @click="reduceCount">减少</button>
+        <button @click="reduceCountAsync">异步减少</button>
+       </p>
+    </div>
+    <div>
+     <p>测试action组合</p>
+     <p>{{currentUser}}</p>
+     <button @click="testActionGroup">点击</button>
     </div>
   </div>
 </template>
@@ -27,20 +48,52 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      reduceSize: 2
+    }
+  },
   components: {
     HelloWorld
   },
   methods: {
+    // 无参提交mutation
     login () {
       this.$store.commit('changeLoginState')
     },
+    // 提交mutations的一种写法,附带参数,不推荐这样写
     changeCount () {
       this.$store.commit('changeCount', 10)
+    },
+    // 提交mutations的另一种写法 推荐
+    reduceCount () {
+      this.$store.commit({
+        type: 'reduceCount',
+        reduceSize: this.reduceSize
+      })
+    },
+    loginAsync () {
+      this.$store.dispatch('loginAsync')
+    },
+    reduceCountAsync () {
+      this.$store.dispatch({
+        type: 'reduceCountAsync',
+        reduceSize: this.reduceSize
+      })
+    },
+    testActionGroup () {
+      this.$store.dispatch({
+        type: 'actionB',
+        index: 1
+      })
     }
   },
   computed: {
     localComputed () { return `this is local computed` },
-
+    // 获取getters中的属性
+    adults () {
+      return this.$store.getters.getAdults
+    },
     // 获取getters里的方法
     getUserById () {
       return this.$store.getters.getUserById
@@ -50,13 +103,20 @@ export default {
     // ...mapState(['isLogin'])      计算属性的名称与state存储的状态名相同时可以使用一个数组参数
 
     ...mapState({
-      count: (state) => state.count,
+      count: 'count',
       isNotLogin: (state) => !state.isLogin,
-      isNotLoginAlias: 'isNotLogin',
-      // 获取getters里的属性
-      adults: 'getAdults'
+      countAlias: 'count',
+      currentUser: 'currentUser'
     })
   }
-
 }
 </script>
+
+<style scope>
+  div{
+    border: 1px solid;
+    margin: 20px;
+    border-radius: 20px;
+    padding: 20px;
+  }
+</style>
